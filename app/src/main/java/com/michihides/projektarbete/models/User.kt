a38330c9a49f09924cb184643adaf346f948f3d5
michihidesugito <Michihides@gmail.com>
2024-04-29 18:43:22 +0200

    fixed so program checks if user already exists in db

---
+++ app/src/main/java/com/michihides/projektarbete/models/User.kt
@@ -0,0 +1,8 @@
+package com.michihides.projektarbete.models
+
+// User Object which contains a username, password and level
+data class User(
+    val username: String,
+    val password: String,
+    val level: Int
+)
--- app/src/main/java/com/michihides/projektarbete/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/michihides/projektarbete/ui/screens/RegisterScreen.kt
@@ -6,8 +6,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.michihides.projektarbete.models.User
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -25,14 +28,26 @@ fun RegisterScreen(
     val toastUserCreated = Toast.makeText(context, "User Created", Toast.LENGTH_SHORT)
     val toastUserExists = Toast.makeText(context, "User Already Exists", Toast.LENGTH_SHORT)
 
+    val user = User("Michan2", "123", 1)
+    val userDatabase = db.child("").child(user.username)
+
     Column {
         // Listener
         Button(onClick = {
-            db.setValue(
-                "NEW VALUE",
-            )
-                .addOnSuccessListener { toastUserCreated.show() }
-                .addOnFailureListener { toastUserExists.show() }
+           userDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
+               override fun onDataChange(snapshot: DataSnapshot) {
+                   if (snapshot.exists()) {
+                       toastUserExists.show()
+                   } else {
+                       userDatabase.setValue(user)
+                       toastUserCreated.show()
+                   }
+               }
+
+               override fun onCancelled(error: DatabaseError) {
+                   TODO("Not yet implemented")
+               }
+           })
         }) {
             Text(text = "DATABASETEST")
         }
