ed0809a5a00674e493b87a465dd63553eac8d0e5
michihidesugito <Michihides@gmail.com>
2024-04-29 19:15:45 +0200

    added dynamic register function

--- app/src/main/java/com/michihides/projektarbete/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/michihides/projektarbete/ui/screens/RegisterScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.michihides.projektarbete.models.User
+import com.michihides.projektarbete.ui.composables.RegisterHandler
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -27,16 +32,29 @@ fun RegisterScreen(
     val context = LocalContext.current
     val toastUserCreated = Toast.makeText(context, "User Created", Toast.LENGTH_SHORT)
     val toastUserExists = Toast.makeText(context, "User Already Exists", Toast.LENGTH_SHORT)
+    val toastEmptyField = Toast.makeText(context, "Please Enter a Username", Toast.LENGTH_SHORT)
+    val toastError = Toast.makeText(context, "Error, try again!", Toast.LENGTH_SHORT)
 
-    val user = User("Michan2", "123", 1)
+    var user by remember {
+      mutableStateOf(
+          User("", "", 1)
+      )
+    }
     val userDatabase = db.child("").child(user.username)
 
     Column {
+        RegisterHandler(
+            user = user,
+            onChangeUser = { user = it }
+        )
+
         // Listener
         Button(onClick = {
            userDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
                override fun onDataChange(snapshot: DataSnapshot) {
-                   if (snapshot.exists()) {
+                   if (user.username == "") {
+                       toastEmptyField.show()
+                   } else if (snapshot.exists()) {
                        toastUserExists.show()
                    } else {
                        userDatabase.setValue(user)
@@ -45,11 +63,11 @@ fun RegisterScreen(
                }
 
                override fun onCancelled(error: DatabaseError) {
-                   TODO("Not yet implemented")
+                   toastError.show()
                }
            })
         }) {
-            Text(text = "DATABASETEST")
+            Text(text = "Register")
         }
     }
 }
